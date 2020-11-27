07a0274ab989b9621b8494325994666fbc1b76b4
amigor31 <72828773+amigor31@users.noreply.github.com>
2020-11-27 18:07:28 -0500

    Oops, we didn't want this commit

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -3,6 +3,7 @@ package hello;
 public class Hello {
 
 	public static void main(String argv[]) {
+		// This is an unwanted but staged comment
 		String name = "World";
 		if (argv.length != 0) {
 			name = argv[0];
