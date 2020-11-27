4a0bdb89d7e8cb493c273b96706fe5303f59e3d8
amigor31 <72828773+amigor31@users.noreply.github.com>
2020-11-27 18:07:41 -0500

    Revert "Oops, we didn't want this commit"
    This reverts commit 07a0274ab989b9621b8494325994666fbc1b76b4.

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -3,7 +3,6 @@ package hello;
 public class Hello {
 
 	public static void main(String argv[]) {
-		// This is an unwanted but staged comment
 		String name = "World";
 		if (argv.length != 0) {
 			name = argv[0];
